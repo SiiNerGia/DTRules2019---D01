34fc97df24e8cfd8db154adc8ddb67ba20d87676
Víctor Rosa <vicrosa25@gmail.com>
2019-02-15 18:17:19 +0100

    Añade query al repo del Admin

--- Acme-Madruga/src/main/java/repositories/AdministratorRepository.java
+++ Acme-Madruga/src/main/java/repositories/AdministratorRepository.java
@@ -4,6 +4,7 @@ package repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Administrator;
@@ -12,5 +13,6 @@ import domain.Administrator;
 @Repository
 public interface AdministratorRepository extends JpaRepository<Administrator, Integer> {
 	
-	
+	@Query("select admin from Administrator admin where admin.userAccount.id = ?1")
+	Administrator findByUserAccountId(int id);
 }
