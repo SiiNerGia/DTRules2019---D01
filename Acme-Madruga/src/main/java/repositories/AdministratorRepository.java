46e295e552b611c13ef2bda0628c729c315a57b4
Víctor Rosa <vicrosa25@gmail.com>
2019-03-04 13:20:38 +0100

    Add query7 to the Admin dashboard

--- Acme-Madruga/src/main/java/repositories/AdministratorRepository.java
+++ Acme-Madruga/src/main/java/repositories/AdministratorRepository.java
@@ -32,18 +32,18 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 
 	@Query("select p from Procession p where p.moment <= ?1")
 	Collection<Procession> query5(Date date);
-
+	
+	@Query("select m.name, count(r), (select count(r2)*0.1 from Request r2) from Member m join m.requests r where r.status = 'APPROVED' group by m having count(r) >= (select count(r2)*0.1 from Request r2)")
+	Collection<Object> query7();
+	
+	
+	
+	
+	
 	// Chart queries
 	@Query("select count(a) from Actor a where a.isSpammer = true")
 	Integer getAllSpammers();
 
 	@Query("select count(a) from Actor a where a.isSpammer = false")
 	Integer getAllNotSpammers();
-
-//	@Query("select m from Member m join m.requests r where 0.1 <= (((count(r) where r.status='APPROVED')*1.0) / (count(r)*1.0))")
-//	Collection<Member> query7();
-
-//	@Query("select p.enrol.size from Position p where p.id = ?1")
-//	Integer query8(int id);
-
 }
