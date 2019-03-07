01a39c02632aa9d196c3703a65dc55ff14e14ae2
Alfredo Campos Duran <alfredocampos4b@gmail.com>
2019-03-07 20:44:46 +0100

    Disenroll funcionando puede

--- Acme-Madruga/src/main/java/repositories/PositionRepository.java
+++ Acme-Madruga/src/main/java/repositories/PositionRepository.java
@@ -1,6 +1,10 @@
+
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Position;
@@ -8,4 +12,6 @@ import domain.Position;
 @Repository
 public interface PositionRepository extends JpaRepository<Position, Integer> {
 
+	@Query("select p from Position p join p.enrols e where e.id=?1")
+	Collection<Position> positionsByEnrol(int enrolId);
 }
--- Acme-Madruga/src/main/java/services/PositionService.java
+++ Acme-Madruga/src/main/java/services/PositionService.java
@@ -1,3 +1,4 @@
+
 package services;
 
 import java.util.Collection;
@@ -7,10 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import repositories.PositionRepository;
 import domain.Actor;
 import domain.Administrator;
+import domain.Enrol;
 import domain.Position;
-import repositories.PositionRepository;
 
 @Service
 @Transactional
@@ -18,63 +20,66 @@ public class PositionService {
 
 	// Manage Repository
 	@Autowired
-	private PositionRepository positionRepository;
+	private PositionRepository	positionRepository;
 
 	// Supporting services
 	@Autowired
-	private ActorService actorService;
-	
+	private ActorService		actorService;
+
 
 	/**
 	 * CRUD methods
 	 */
 	public Position create() {
-		Position result = new Position();
-//		Collection<Enrol> enrol = new ArrayList<Enrol>();
-//		result.setEnrol(enrol);
+		final Position result = new Position();
+		//		Collection<Enrol> enrol = new ArrayList<Enrol>();
+		//		result.setEnrol(enrol);
 
 		return result;
 	}
 
-	public Position findOne(int id) {
-		Position result = this.positionRepository.findOne(id);
+	public Position findOne(final int id) {
+		final Position result = this.positionRepository.findOne(id);
 		Assert.notNull(result);
 
 		return result;
 	}
 
 	public Collection<Position> findAll() {
-		Collection<Position> result = this.positionRepository.findAll();
+		final Collection<Position> result = this.positionRepository.findAll();
 
 		Assert.notNull(result);
 
 		return result;
 	}
 
-	public Position save(Position position) {
+	public Position save(final Position position) {
 		Assert.notNull(position);
 		Actor principal;
-		
+
 		// Principal must be an Admin
 		principal = this.actorService.findByPrincipal();
 		Assert.isInstanceOf(Administrator.class, principal);
-		
+
 		return this.positionRepository.save(position);
 	}
-	
-	
-	public void delete(Position position) {
+
+	public void delete(final Position position) {
 		Assert.notNull(position);
 		Actor principal;
-		
+
 		// Principal must be an Admin
 		principal = this.actorService.findByPrincipal();
 		Assert.isInstanceOf(Administrator.class, principal);
 		Assert.isTrue(position.getId() != 0);
-		
+
 		// Posistion must not be in use to be deleted
 		Assert.isTrue(position.getEnrol().isEmpty());
-		
+
 		this.positionRepository.delete(position);
+	}
+
+	public Collection<Position> positionsByEnrol(final Enrol enrol) {
+		return this.positionRepository.positionsByEnrol(enrol.getId());
 	}
 }
